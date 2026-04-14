a5dd92dfe6a4c06a450d797a2a184e5d68955090
RholsingEMU <ravi.holsinger@emu.edu>
2026-04-14 14:52:14 -0400

    Changed home page and added email styling files

--- lost-and-found/src/pages/HomePage.jsx
+++ lost-and-found/src/pages/HomePage.jsx
@@ -7,12 +7,16 @@ function HomePage() {
     <>
       <HeroBanner title="EMU Lost & Found" />
       <main className="container">
-        <p className="home-intro">
-          Welcome to Eastern Mennonite University&apos;s Lost & Found. Report lost items or browse found items below.
-        </p>
+        <div className="home-card">
+          <center>
+            <p className="item-detail-content">Welcome to Eastern Mennonite University&apos;s Lost & Found!</p>
+            <p className="item-detail-content" style={{ fontSize: '12px', paddingTop: '0px' }}>This tool will help you find lost items on campus</p>
+          </center>
+        </div>
         <div className="home-actions">
           <Link to="/lost-items" className="btn-primary">Browse Lost Items</Link>
-          <Link to="/lost-submit-form" className="btn-secondary">Report a Lost Item</Link>
+          <Link to="/lost-submit-form" className="btn-secondary">Browse Found Items</Link>
+          <Link to="/lost-submit-form" className="btn-tertiary">Report a Lost Item</Link>
         </div>
       </main>
     </>
